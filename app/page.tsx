313fc0791d6d2e542fd46de87d8351a79d89f6ab
ColaWizard <aleksandarsubotic@gmail.com>
2024-11-15 15:54:24 +0100

    Assets and Metadata

--- app/page.tsx
+++ app/page.tsx
@@ -1,2 +1,7 @@
-const Home = () => <h1 className="h1-bold">Welcome</h1>;
+const Home = () => (
+  <>
+    <h1 className="h1-bold">Welcome</h1>
+    <h1 className="h1-bold font-space-grotesk">Welcome 22</h1>
+  </>
+);
 export default Home;
